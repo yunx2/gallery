db326d4dde0c6745140b215b3f6e4efe4b86eefe
Joel <joelkaplan1@gmail.com>
2019-07-07 10:04:23 -0700

    Initialize css with webpack config

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,19 +4,28 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+    };
+
+    this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
+    this.getData();
+  }
+
+
+  getData() {
     //const listing = window.location.href.split('/')[4];
     axios.get('/api/1/images')
       .then((response) => {
-        console.log(response);
+        this.setState({ data: response.data });
       })
       .catch((err) => {
         console.log(err);
       });
   }
+
 
   render() {
     return (
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './style.css';
+
 
 ReactDOM.render(
   <App />, document.getElementById('root'),
