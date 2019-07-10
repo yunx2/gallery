7b0717d6636ef3598ca04b6cd99dc97b3dfbede7
Joel <joelkaplan1@gmail.com>
2019-07-10 14:29:28 -0700

    Fix minor className bug on hover

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = (props) => {
   const { ImageUrl, imageClickHandler, imageHoverHandler, imageUnHoverHandler, ImageID, hoverClass } = props;
-  return <img onClick={ () => { imageClickHandler(ImageID) } } onMouseEnter={ ()=> imageHoverHandler(ImageID) } onMouseLeave={ () => imageUnHoverHandler() } className="gallery-image" className={hoverClass} alt="listing" src={ImageUrl} />;
+  return <img onClick={ () => { imageClickHandler(ImageID) } } onMouseEnter={ ()=> imageHoverHandler(ImageID) } onMouseLeave={ () => imageUnHoverHandler() } className={`gallery-image ${hoverClass}`} alt="listing" src={ImageUrl} />;
 };
 
 export default Image;
