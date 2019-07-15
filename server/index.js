b7f475fc7e00c56bc2e82f9d430c1b2f92ec692d
Joel <joelkaplan1@gmail.com>
2019-07-15 16:53:56 -0700

    Standardize title and use path to resolve relative paths

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
 const controllers = require('./controllers/index');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3003;
 
 const db = require('../database/index.js');
 
-app.use('/rooms/:listingid', express.static('../public'));
+app.use('/rooms', express.static(path.join(__dirname, '../public')));
 
 app.get('/api/:listingid/images', controllers.getImages);
 
