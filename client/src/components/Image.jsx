5971070957e8b792725bbfdf0f6a4b50ad01fc2e
Joel <joelkaplan1@gmail.com>
2019-07-15 16:06:07 -0700

    Fix API

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import style from '../style.css';
 
 const Image = (props) => {
-  const { ImageUrl, imageClickHandler, imageHoverHandler, imageUnHoverHandler, ImageID, hoverClass } = props;
+  const {
+    ImageUrl,
+    imageClickHandler,
+    imageHoverHandler,
+    imageUnHoverHandler,
+    ImageID,
+    hoverClass,
+  } = props;
 
   return <img onClick={ () => { imageClickHandler(ImageID) } } onMouseEnter={ imageHoverHandler && (() => imageHoverHandler(ImageID)) } onMouseLeave={ imageUnHoverHandler && (() => imageUnHoverHandler()) } className={style.galleryImage + ' ' + hoverClass} alt="listing" src={ImageUrl} />;
 };
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const controllers = require('./controllers/index');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 const db = require('../database/index.js');
 
