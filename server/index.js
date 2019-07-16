2069964b24d7d9fc211e03d55b84169ddf68b8a1
Joel <joelkaplan1@gmail.com>
2019-07-15 18:55:57 -0700

    Include CORS

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
+const cors = require('cors');
 const controllers = require('./controllers/index');
 const path = require('path');
 
@@ -10,6 +11,6 @@ const db = require('../database/index.js');
 
 app.use('/rooms/:listingid', express.static(path.join(__dirname, '../public')));
 
-app.get('/api/:listingid/images', controllers.getImages);
+app.get('/api/:listingid/images', cors(), controllers.getImages);
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
